12692fbb12347a85778138041112648a133be540
Foucsi <julien.foucartreact@gmail.com>
2023-01-14 21:17:04 +0100

    mise en place des image users

--- components/Profils.js
+++ components/Profils.js
@@ -1,10 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 
-export default function Profils({ profil }) {
+export default function Profils({ profil, images }) {
   return (
     <View style={styles.profil}>
-      <Text>{profil.username}</Text>
+      <View style={{ height: "70%", width: "100%" }}>
+        <Image
+          style={{ height: "100%", width: "100%" }}
+          source={{ uri: images[Math.floor(Math.random() * images.length)] }}
+        />
+      </View>
+      <View style={{ height: "30%", width: "100%", backgroundColor: "tomato" }}>
+        <Text>{profil.username}</Text>
+      </View>
     </View>
   );
 }
