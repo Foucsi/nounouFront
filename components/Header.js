f92eaea0c98bbd5d1e2d3b180615c6ca42ab279e
Foucsi <julien.foucartreact@gmail.com>
2023-01-15 11:52:13 +0100

    updtate header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,40 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux";
+import { Feather } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function Header() {
+export default function Header({ navigation }) {
   const users = useSelector((state) => state.user.value);
   return (
     <View style={styles.header}>
-      <Text>Welcome {users.username}</Text>
+      <View
+        style={{ alignItems: "center", flexDirection: "row", width: "80%" }}
+      >
+        <View
+          style={{
+            width: "20%",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity>
+            <Feather name="menu" size={48} color="black" />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            width: "65%",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+            <MaterialIcons name="child-care" size={48} color="#1282A2" />
+          </TouchableOpacity>
+          <Text style={{ fontWeight: "bold" }}>
+            <Text style={{ color: "#1282A2" }}>SKI</Text>NOUNOU
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,10 +43,11 @@ const styles = StyleSheet.create({
   header: {
     width: "100%",
     height: "15%",
-    alignItems: "center",
-    justifyContent: "flex-end",
+    justifyContent: "space-around",
+    alignItems: "flex-end",
     paddingBottom: 20,
     borderBottomColor: "grey",
     borderBottomWidth: 1,
+    flexDirection: "row",
   },
 });
